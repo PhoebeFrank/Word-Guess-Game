660a0f5806449279db476e6b5ad1be4503e9e6be
Phoebe Frank <pfrank1991@gmail.com>
2018-12-22 22:25:10 -0500

    My Changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
-//Create variables
+//Create all variables needed
 var bandNames = ["nirvana","weezer","soundgarden","radiohead","metallica","oasis","nsync","sublime","everclear"];
 var numberLetters = [];
 var numberSpaces = 0;
-var spacesLetters = [];
+var spacesForLetters = [];
 var totalGuesses = 10
 var wrong = [];
 var guesses = "";
@@ -11,31 +11,35 @@ var losses = 0;
 var wordGenerator = ""; 
 var letterExists //need to figure this out still
 
+//code what happens when game starts and how the screen should look
 function beginGame(){
     totalGuesses = 10;
-    //randomly choose a band from array
+    //randomly choose a band from array of choices for guess
     wordGenerator = bandNames[Math.floor(math.random()* bandNames.length)];
-        //split the word into letters for later
+        //split the word into letters that will be used for blanks on screen and array for guessing against
         numberLetters=wordGenerator.split("");
         //count number of letters for the blanks on screen
         numberSpaces=numberLetters.length;
         
         console.log(wordGenerator);
-        //reset all of the counters
+        //reset all of the counters for when game starts
         wrong = [];
         spacesLetters = [];
     //create correct number of blanks on screen for guessing
     for (var i=0; i < numberSpaces; i++){
-        spacesLetters.push("_");
+        spacesForLetters.push("_");
     }
         
-        console.log(spacesLetters);
+        console.log(spacesForLetters);
 
     //show number of blanks for guessing
+        document.getElementById("letters").innerHTML = spacesForLetters.join(" ");
     //show amount of letters left to guess
+         document.getElementById("guesses").innerHTML = totalGuesses;
     //clear screen
+        document.getElementById("wrongLetters").innerHTML = wrong.join(" ");
 
-
+}
     //need to create functions for checking guessed letters against the array of letters in each word (variables listed in variable list at top of page)
     
     //if letter exists in word, fill in the blank 
@@ -44,4 +48,3 @@ function beginGame(){
     //restart game
     //if user runs out of allowed letters before guessing word, add one to losses, tell them they lost
     //restart game
-}
